ccb15508182c9b0060c55f54deee6aa38d297ccc
Suyash Pandey <sayashpandey30@gmail.com>
2024-12-15 23:14:14 +0530

    taskease-mini

---
+++ index.js
@@ -0,0 +1,46 @@
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  const cardId = event.dataTransfer.getData("text");
+  const cardElement = document.getElementById(cardId);
+  const dropzone = event.target;
+  dropzone.appendChild(cardElement);
+}
+
+function addnewTask(event) {
+  event.preventDefault();
+  var task = prompt("Enter new Task");
+  if (task) {
+    const elements = document.getElementsByClassName("draggable-card");
+    var len = elements.length + 1;
+    const tasknew = document.createElement("div");
+    tasknew.setAttribute("id", len);
+    tasknew.classList.add("draggable-card");
+    tasknew.setAttribute("draggable", "true");
+    tasknew.setAttribute("ondragstart", "drag(event)");
+    const deletebtn = `<div id="part2"><a href="#" onClick="deletetask(event)" class="${len}"><h5 class="${len}">Delete</h5><div id="deleteclr"></div></a></div>`;
+    tasknew.innerHTML = task + deletebtn;
+    const column1 = document.getElementById("column1");
+    column1.appendChild(tasknew);
+  }
+}
+
+function deletetask(event) {
+  event.preventDefault();
+  var deleteId = event.target.classList;
+  console.log(deleteId[0]);
+  const tasks = document.querySelectorAll(".draggable-card");
+  console.log(tasks);
+  tasks.forEach((e) => {
+    if (e.id === deleteId[0]) {
+      e.remove();
+    }
+  });
+}
